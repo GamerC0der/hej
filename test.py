d76dde796daddf6e0346551db79ad89acf177bb5
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2025-11-17 16:22:50 -0800

    404 HTML

--- test.py
+++ test.py
@@ -2,11 +2,26 @@ import hej
 
 @get('/')
 def home():
-    return 'home.html'
+    return html.html(
+        html.head(
+            html.title('Hej')
+        ),
+        html.body(
+            html.h1('Hej, World')
+        )
+    ) 
 
 @not_found
 def custom_404():
-    return '<h1> Not Found</h1><p>This page doesn\'t exist!</p>'
+    return html.html(
+        html.head(
+            html.title('404 Not Found')
+        ),
+        html.body(
+            html.h1('404 Not Found'),
+            html.p('This page doesn\'t exist!')
+        )
+    )
 
 if __name__ == '__main__':
     hej.run(debug=True)
